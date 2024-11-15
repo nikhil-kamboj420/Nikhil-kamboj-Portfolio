7c9ae7b89337db6473e4528bcef68b9983ea91ee
NIKHIL KAMBOJ <kambojnikhil44@gmail.com>
2024-11-15 17:12:08 +0530

    change the font size for small devices

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    main: ['./src/js/main.js', './src/css/styles.scss'],
+    main: ['./main.js', './styles.scss'],
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist/js')
+    path: path.resolve(__dirname, './js')
   },
   module: {
     rules: [
